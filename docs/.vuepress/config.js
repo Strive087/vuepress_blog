2571c49648ea2ac8305e5c8dc2ee1663bf70b4ed
”zhuduanlei“ <840459919@qq.com>
2020-11-11 15:47:39 +0800

    create

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,85 @@
+const autobar_options = {
+  maxLevel : 2,
+  navPrefix : "nav",
+  skipEmptySidebar : true,
+  skipEmptyNavbar : true,
+  multipleSideBar : true,
+  pinyinNav:true
+};
+// 配置文件的入口文件
+module.exports = {
+  title: "前端之路",
+  description: "Just playing around",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    // !禁用导航栏
+    // navbar: false,
+    //导航栏logo
+    // logo: "/assets/img/road.png",
+    //导航栏链接
+    nav: [],
+    // //------------------------------------
+    //侧边栏
+    sidebar: [
+      {
+        title: 'Engineering',   // 必要的
+        path: '/Engineering/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 3,    // 可选的, 默认值是 1
+        children: [
+          '/Engineering/webpack/'
+        ]
+      },
+    ],
+    //侧边栏自动生成
+    // sidebar: "auto",
+    //显示所有页面的标题链接
+    // displayAllHeaders: true,
+    // //------------------------------------
+    //!禁用搜索
+    // search: false,
+    //搜索显示的结果数量
+    searchMaxSuggestions: 10,
+    // //------------------------------------
+    //基于git提交时间显示最后更新时间
+    // lastUpdated: 'Last Updated',
+    // //------------------------------------
+    // 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
+    // nextLinks: false,
+    // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
+    // prevLinks: false
+    // //------------------------------------
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: "vuejs/vuepress",
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: "GitHub",
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: "vuejs/vuepress",
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: "docs",
+    // 假如文档放在一个特定的分支下：
+    docsBranch: "master",
+    // 默认是 false, 设置为 true 来启用
+    // editLinks: true,
+    // 默认为 "Edit this page"
+    // editLinkText: '帮助我们改善此页面！',
+    // //------------------------------------
+    //添加页面滚动效果
+    smoothScroll: true
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // '@alias': 'path/to/some/dir'
+      }
+    }
+  },
+  plugins:[ 'permalink-pinyin', ['autobar',autobar_options]]
+};
+
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /assets/img/maliao.jpeg
+heroText: 前端之路
+tagline: 路漫漫其修远兮
+actionText: 学习笔记 →
+actionLink: /front_end/
+features:
+- title: 重建博客
+  details: 2020.11.09从Hexo迁移到VuePress
+- title: 探索前端
+  details: 继续探索前端技术,享受学习的乐趣
+- title: 记录沉淀
+  details: 日常记录学习笔记,消化沉淀技术知识
+# footer: 前端之路 | Copyright © 2020 Zhu DL
+---
