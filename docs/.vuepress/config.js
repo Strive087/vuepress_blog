4901ffbf9eb58fd47fea4eb32f9a047ff98ee779
”zhuduanlei“ <840459919@qq.com>
2021-04-09 11:32:18 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,19 @@ module.exports = {
   description: "Just playing around",
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }],
-    ["meta", { name: "viewport", content: "width=device-width,initial-scale=1" }]
+    [
+      "meta",
+      { name: "viewport", content: "width=device-width,initial-scale=1" }
+    ],
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     // !禁用导航栏
@@ -72,6 +84,11 @@ module.exports = {
     ["autobar", { pinyinNav: true }],
     "@vuepress/back-to-top",
     "@vuepress/medium-zoom",
-    "rpurl"
+    "rpurl",
+    "@vuepress/pwa",
+    {
+      serviceWorker: true,
+      updatePopup: false
+    }
   ]
 };
